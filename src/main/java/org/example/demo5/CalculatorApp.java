0ad66f4aa378181d8984e7e899e643cdfdf60b6d
Rodeeyah <rodiyahmusa@gmail.com>
2025-08-05 09:35:28 -0700

    https://github.com/Rodeeyah05/JavaFx.git

---
+++ src/main/java/org/example/demo5/CalculatorApp.java
@@ -0,0 +1,12 @@
+package org.example.demo5;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CalculatorApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
